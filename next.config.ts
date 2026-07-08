55f09b4fe70dbb308d4d4188f27530164badefa9
Tawiah Obed <tawiaho858@gmail.com>
2026-07-08 15:15:08 +0000

    added in the  two  IP address in the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Move it out of experimental to the top level
-  allowedDevOrigins: ['172.20.10.2', 'localhost:3000'],
+  allowedDevOrigins: [
+  "localhost",
+  "127.0.0.1",
+  "172.20.10.2",
+  "30.30.100.163",
+],
   reactStrictMode: true,
 };
 
